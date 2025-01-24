fb9e5191fd1000148f919b80d0c2a15e9634ba6b
Ooogeid <diegomb972@gmail.com>
2025-01-24 10:45:23 +0100

    button en rouge

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -11,7 +11,7 @@ export default function Navbar({ username }: { username: string }) {
 		<nav className="flex items-center justify-between py-4">
             <div className='flex items-center space-x-4'>
                 <h1 className="text-3xl font-bold me-10">Alertes</h1>
-                <Button className='cursor-pointer hover:text-white hover:bg-red-700'>Ajouter un sinistre</Button>
+                <Button onClick={() => {}} className='cursor-pointer bg-red-600 text-white hover:bg-white hover:text-red-600'>Ajouter un sinistre</Button>
                 <Button className='cursor-pointer hover:bg-gray-200 text-sm'>Créer une activité</Button>
             </div>
             {username &&
@@ -20,7 +20,6 @@ export default function Navbar({ username }: { username: string }) {
                     <Button onClick={handleDisconnect} className='cursor-pointer hover:bg-gray-200'>Se déconnecter</Button>
                 </div>
             }
-
 		</nav>
 	);
 }
